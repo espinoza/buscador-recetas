07cc793a81ece1ea692d178c7986d66368f466d5
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-07-20 18:22:13 -0400

    Feature deleted: add or edit recipes by users manually

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -3,14 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('new/',
-         views.NewRecipeView.as_view(),
-         name='new_recipe'),
-
-    path('<int:recipe_id>/edit/',
-         views.EditRecipeView.as_view(),
-         name='recipe_edition'),
-
     path('<int:recipe_id>/',
          views.RecipeDetailView.as_view(),
          name='view_recipe'),
