ee2490dc1e1c9757d2a5fd0d15e9e133dc0f91e2
Jorge Espinoza <jorgeespinozaortega@gmail.com>
2021-07-28 00:14:21 -0400

    Detect ingredients for all recipes automatically

--- apps/ingredients/urls.py
+++ apps/ingredients/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
          views.CheckRecipeIngredientsView.as_view(),
          name='check_recipe_ingredients'),
 
+    path('detect_ingredients_for_all_recipes',
+         views.detect_ingredients_for_all_recipes,
+         name='detect_ingredients_for_all_recipes'
+    ),
+
     path('add_new_to_recipe/<int:recipe_id>',
          views.AddNewIngredientView.as_view(),
          name='add_new_ingredient_to_recipe'),
--- apps/ingredients/views.py
+++ apps/ingredients/views.py
@@ -17,13 +17,14 @@ class CheckRecipeIngredientsView(LoginRequiredMixin, FormView):
     def dispatch(self, request, *args, **kwargs):
         if request.user.level != 1:
             return redirect("/")
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
         recipe = Recipe.objects.filter(id=self.kwargs['recipe_id'])
         if not recipe:
             return redirect("/")
-        recipe = recipe[0]
+        self.recipe = recipe[0]
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        recipe = self.recipe
         detect_ingredients(recipe)
         context = super().get_context_data(**kwargs)
         context.update({
@@ -96,25 +97,52 @@ class AddNewIngredientView(LoginRequiredMixin, CreateView):
                             kwargs={"recipe_id": recipe_id})
 
 
+def detect_ingredients_for_all_recipes(request):
+    if not request.user.is_authenticated or request.user.level != 1:
+        return redirect("/")
+    recipes = Recipe.objects.all()
+    for recipe in recipes:
+        detect_ingredients(recipe)
+    return redirect("update_recipe_database")
+
+
 def detect_ingredients(recipe):
-    special_characters = ",.;:¡!¿?()/"
+    """Reads ingredient lines in a recipe to search for ingredient names
+    and saves many to many relationship between the recipe and corresponding
+    ingredient objects.
+    """
+    recipe.ingredients.clear()
+    SPECIAL_CHARACTERS = ",.;:¡!¿?()/"
+
     for ingredient_line in recipe.ingredient_lines.all():
         line = ingredient_line.text.lower()
-        for char in special_characters:
-            line.replace(char, "&")
-        phrases = re.split(r'[&]', line)
+
+        # Ingredient line as a list of phrases without special characters
+        for char in SPECIAL_CHARACTERS:
+            line = line.replace(char, "&")
+        phrases = [phrase.strip(" ") for phrase in re.split(r'&|\sy\s', line)
+                                     if re.search(r'\w', phrase)]
+
         for phrase in phrases:
-            words = phrase.split(" ")
-            for start in range(len(words)):
+            # Analyse concatenations of words that could be ingredient names
+            words = [word for word in phrase.split(" ")
+                          if not word.isnumeric()]
+
+            # The ingredient name is more likely to appear to the right
+            for start in range(len(words)-1, -1, -1):
                 end = len(words)
                 ingredient_found = False
+
                 while not ingredient_found and end > start:
                     mini_phrase = " ".join(words[start:end])
                     ingredients = Ingredient.objects.filter(
                         Q(names__singular=mini_phrase)
                         | Q(names__plural=mini_phrase)
                     )
+
                     if ingredients:
                         ingredient_found = True
                         recipe.ingredients.add(*ingredients)
-                    end -= 1
+                        end = start
+                    else:
+                        end -= 1
